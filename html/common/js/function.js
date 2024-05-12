f6db6cae35f1770b7d907cd2cdac0de388d2a13e
nkawamot <n0z0me.2ca@gmail.com>
2024-05-12 12:59:22 +0900

    Add: modal window for renewal

--- html/common/js/function.js
+++ html/common/js/function.js
@@ -41,3 +41,53 @@ $(function() {
 $(document).ready(function() {
   $('.drawer').drawer();
 });
+
+
+// Modal Window for Renewal
+const sessionKey = "accessed";
+const sessionValue = true;
+const modal = `
+<div id="modal" class="modal-container active">
+<div class="modal-body">
+    <div class="modal-close">×</div>
+    <div class="modal-content">
+        <h1>ホテルリニューアルに伴う休業のお知らせ</h1>
+        <div>
+            <p class="p1">
+                いつも三段峡ホテルをご利用頂き誠にありがとうございます。<br>
+                誠に勝手ながら改装工事のため2024年3月より一時休業とさせていただきます。<br>
+                営業再開日についてはまた改めてご案内します。<br>
+                お客様には大変ご不便をお掛けいたしますが、ご了承下さいますようお願い申し上げます。<br>
+                改装後も、引き続きご愛顧の程よろしくお願いいたします。
+            </p>
+            <p class="p2">
+              お問い合わせは、下記メールアドレスまでお願いいたします。
+            </p>
+            <p class="contact-email">contact@sandankyo.co.jp</p>
+        </div>
+    </div>
+</div>
+`;
+$(document).ready(function() {
+  if (!sessionStorage.getItem(sessionKey)) {
+    $('body').append(modal);
+    sessionStorage.setItem(sessionKey, sessionValue);
+    var close = $('.modal-close');
+    var container = $('.modal-container');
+    // Close window if the close button of the window was clicked.
+	close.on('click',function(){	
+		container.removeClass('active');
+	});
+    // Close window if the outside of the window was clicked.
+	$(document).on('click',function(e) {
+		if(!$(e.target).closest('.modal-body').length) {
+			container.removeClass('active');
+		}
+	});
+  } else {
+    var container = $('.modal-container');
+    if (container) {
+        container.removeClass('active');
+    }
+  }
+});
